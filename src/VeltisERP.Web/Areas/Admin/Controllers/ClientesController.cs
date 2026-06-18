158fc6f9dccdbd2cdf8894507e3421f1b7907610
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-06-17 22:06:21 -0300

    feat: implementa CRUD de clientes na area administrativa

--- src/VeltisERP.Web/Areas/Admin/Controllers/ClientesController.cs
+++ src/VeltisERP.Web/Areas/Admin/Controllers/ClientesController.cs
@@ -1,27 +1,165 @@
-using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using VeltisERP.Domain.Entities;
 using VeltisERP.Infrastructure.Data;
 
-namespace VeltisERP.Web.Areas.Admin.Controllers;
-
-[Area("Admin")]
-[Authorize]
-public class ClientesController : Controller
+namespace VeltisERP.Web.Areas.Admin.Controllers
 {
-    private readonly AppDbContext _context;
-
-    public ClientesController(AppDbContext context)
+    [Area("Admin")]
+    public class ClientesController : Controller
     {
-        _context = context;
-    }
+        private readonly AppDbContext _context;
 
-    public async Task<IActionResult> Index()
-    {
-        var clientes = await _context.Clientes
-            .Include(x => x.Empresa)
-            .ToListAsync();
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-        return View(clientes);
+        // GET: Admin/Clientes
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Clientes.Include(c => c.Empresa);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .Include(c => c.Empresa)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Admin/Clientes/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "NomeFantasia");
+            return View();
+        }
+
+        // POST: Admin/Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NomeRazaoSocial,Documento,Email,Telefone,Endereco,Cidade,Estado,Cep,EmpresaId,Id,Ativo,DataCadastro,DataAlteracao")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "NomeFantasia", cliente.EmpresaId);
+            return View(cliente);
+        }
+
+        // GET: Admin/Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "NomeFantasia", cliente.EmpresaId);
+            return View(cliente);
+        }
+
+        // POST: Admin/Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NomeRazaoSocial,Documento,Email,Telefone,Endereco,Cidade,Estado,Cep,EmpresaId,Id,Ativo,DataCadastro,DataAlteracao")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpresaId"] = new SelectList(_context.Empresas, "Id", "NomeFantasia", cliente.EmpresaId);
+            return View(cliente);
+        }
+
+        // GET: Admin/Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .Include(c => c.Empresa)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Admin/Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente != null)
+            {
+                _context.Clientes.Remove(cliente);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Clientes.Any(e => e.Id == id);
+        }
     }
 }
